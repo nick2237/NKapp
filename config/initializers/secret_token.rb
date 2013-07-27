5784f562194e184363351ce39ac6203be88c113f
Nicholas <nksiezopolski@gmail.com>
2013-07-27 14:45:29 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NKapp::Application.config.secret_token = '94c95d6e4b79f2e8553b0ee8effd1c2b7d5798480e8594a3cbdaac4b72d7b74dbe6860029e9b84f40c3bca79a3681ba1cc894fc0062bf0605689b9ddd00e6210'
